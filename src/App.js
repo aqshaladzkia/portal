3dcd6c0d85e2dd084bf6b0bbea9e637ab7296225
Muhammad Aqshal Adzkia <bacaberdua@gmail.com>
2023-11-29 23:49:32 +0700

    update hari ini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, { createContext, useEffect, useState } from "react";
+import React from "react";
 import { Navbar } from "./Components/Navbar/Navbar";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard";
+import Login from "./Pages/login";
 
 function App() {
   return (
     <Router>
-      <div className="position-relative">
-        <Navbar />
-        <div className="wrapper-content">
-          <Sidebar />
-          <div className="w-100">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={
+          <>
+            <div className="position-relative">
+              <Navbar />
+              <div className="wrapper-content">
+                <Sidebar />
+                <div className="w-100">
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </>
+        }/>
+      </Routes>
     </Router>
   );
 }
